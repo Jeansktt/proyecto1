9bb4d3cf616a3214e91b6e3bc80eba2b23241bfc
Kxflt <arg.uri.solanes@gmail.com>
2023-04-24 13:15:47 +0200

    archivos pasados por isma

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+'use strict';
+//aqui se extraera el JSON.
+
+//aqui se selecciona el h2 para imprimir las preguntas, estoes solo para seleccionarlos.
+const preguntas = document.querySelector('#pregunta');
+//ahora seleccionamos los botones.
+//primer boton
+const buttonOne = document.querySelector('#optionOne');
+//segundo boton
+const buttonTwo = document.querySelector('#optionTwo');
+//tercer boton
+const buttonThree = document.querySelector('#optionThree');
+//cuarto boton
+const buttonFour = document.querySelector('#optionFour');
